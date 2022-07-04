e347b34d546b7bdead4815ce379433575104c2f8
Aayush <aayush5521186@gmail.com>
2022-07-04 19:19:03 +0530

    changes for production

--- backend/app.js
+++ backend/app.js
@@ -42,12 +42,6 @@ if (process.env.NODE_ENV == "production") {
 }
 
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html")));
-}
-
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Backend is running on port ${port}`);
